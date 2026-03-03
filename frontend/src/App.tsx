dbdbdd322d85b09d6f29a5febe0ee65ca90719de
tutuatututua <nadhee.jira.tua@gmail.com>
2026-03-03 18:31:59 +0700

    tickerv3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ import RecommendationPage from './pages/RecommendationPage'
 import TickerPage from './pages/TickerPage'
 import VideoInsightsPage from './pages/VideoInsightsPage'
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import React from 'react'
 import { getBackendBaseUrl } from './config/env'
 
